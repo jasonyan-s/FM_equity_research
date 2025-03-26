c90a9f935ea3de65381f792721c34111480b2ea0
kcathyzou <kexin.zou@finma.au>
2025-03-26 08:22:06 +0000

    git commit -m "Add cathy.py"

--- cathy.py
+++ cathy.py
@@ -1,1 +1,36 @@
-#ggg
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+# URL of the Commonwealth Bank Wikipedia page
+commbank_url = "https://en.wikipedia.org/wiki/Commonwealth_Bank"
+
+# Scrape tables using pandas
+tables = pd.read_html(commbank_url)
+
+# Assuming you want the first table on the page
+commbank_table = tables[0]
+print("Table:")
+print(commbank_table.head())
+
+# Scrape the image using BeautifulSoup
+response = requests.get(commbank_url)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+# Find the first image in the infobox (usually the logo or key image)
+infobox = soup.find("table", {"class": "infobox"})
+if infobox:
+    image_tag = infobox.find("img")
+    if image_tag:
+        image_url = "https:" + image_tag["src"]
+        print("Image URL:", image_url)
+
+        # Download the image (optional)
+        image_response = requests.get(image_url)
+        with open("commbank_logo.jpg", "wb") as file:
+            file.write(image_response.content)
+        print("Image saved as commbank_logo.jpg")
+    else:
+        print("No image found in the infobox.")
+else:
+    print("No infobox found on the page.")
